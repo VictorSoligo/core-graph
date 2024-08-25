6d812320bd7b725f8dcf0d97919fad5b661bd805
Victor Hugo Soligo <victorsoligo@gmail.com>
2024-08-25 12:50:01 -0300

    chore: add zoom and move buttons to toolbar

---
+++ src/components/toolbar-separator.tsx
@@ -0,0 +1,5 @@
+import { Separator } from './ui/separator'
+
+export function ToolbarSeparator() {
+  return <Separator orientation="vertical" className="h-6" />
+}
--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -1,9 +1,18 @@
-import { Eraser, Plus } from 'lucide-react'
+import { useDisplayList } from '@/contexts/display-list-context'
+import {
+  Eraser,
+  MoveDown,
+  MoveLeft,
+  MoveRight,
+  MoveUp,
+  Plus,
+  ZoomIn,
+  ZoomOut,
+} from 'lucide-react'
 import { useState } from 'react'
 import { ShapeModal } from './shape-modal'
+import { ToolbarSeparator } from './toolbar-separator'
 import { Button } from './ui/button'
-import { Separator } from './ui/separator'
-import { useDisplayList } from '@/contexts/display-list-context'
 
 export function Toolbar() {
   const [isShapeModalOpen, setIsShapeModalOpen] = useState(false)
@@ -18,13 +27,45 @@ export function Toolbar() {
     <>
       <header className="h-[72px] px-4 w-full flex gap-4 items-center bg-white border-b-2">
         <Button size="icon" onClick={handleOpenShapeModal}>
-          <Plus className="h-5 w-5 text-white" />
+          <Plus className="size-5 text-white" />
         </Button>
 
-        <Separator orientation="vertical" className="h-6" />
+        <ToolbarSeparator />
+
+        <div className="flex gap-2">
+          <Button size="icon" variant="secondary">
+            <MoveLeft className="size-5" />
+          </Button>
+
+          <Button size="icon" variant="secondary">
+            <MoveRight className="size-5" />
+          </Button>
+
+          <Button size="icon" variant="secondary">
+            <MoveUp className="size-5" />
+          </Button>
+
+          <Button size="icon" variant="secondary">
+            <MoveDown className="size-5" />
+          </Button>
+        </div>
+
+        <ToolbarSeparator />
+
+        <div className="flex gap-2">
+          <Button size="icon" variant="secondary">
+            <ZoomOut className="size-5" />
+          </Button>
+
+          <Button size="icon" variant="secondary">
+            <ZoomIn className="size-5" />
+          </Button>
+        </div>
+
+        <ToolbarSeparator />
 
         <Button size="icon" variant="destructive" onClick={clearDisplayList}>
-          <Eraser className="h-5 w-5 text-white" />
+          <Eraser className="size-5 text-white" />
         </Button>
       </header>
 
